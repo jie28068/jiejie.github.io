eb8364e5ac02a04eb81fec2e2764992072acde29
shanjie <2583782734@qq.com>
2024-12-29 23:12:08 +0800

    update

--- js/page.js
+++ js/page.js
@@ -12,7 +12,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('戒戒同学|生日快乐|happy birthday|#countdown 3|');
+      S.UI.simulate('戒戒|生日快乐|happy birthday|#countdown 3|');
     }
 
     S.Drawing.loop(function () {
